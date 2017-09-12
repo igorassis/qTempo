08d9ea9e06cabcf1288da901a57f9803130ec682
igorassis <ipires_@hotmail.com>
2017-09-12 02:52:23 -0300

    upload

---
+++ src/providers/weather/weather.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+
+@Injectable()
+export class WeatherProvider {
+
+  apiKey = '63e0497ba9cc30fa';
+  url;
+
+  constructor(public http: Http) {
+    console.log('Hello WeatherProvider Provider');
+    this.url = 'http://api.wunderground.com/api/'+this.apiKey+'/lang:BR/conditions/q';
+  }
+
+  getWeather(city, state){
+    return this.http.get(this.url+'/'+state+'/'+city+".json")
+      .map(res => res.json());
+  }
+
+}
